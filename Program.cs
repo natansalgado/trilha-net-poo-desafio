370cb19083927966e775a4b872fd523d712b926d
Natan Salgado <natannascimentosalgado@gmail.com>
2024-01-15 22:11:37 -0300

    Desafio Concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "11 92623-4536", modelo: "Modelo 32", imei: "3427542", memoria: 32);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Discord");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "11 99341-9683", modelo: "Modelo 12", imei: "9853241", memoria: 64);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Facebook");
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+### Desafio concluído
+
 # DIO - Trilha .NET - Programação orientada a objetos
 www.dio.me
 
